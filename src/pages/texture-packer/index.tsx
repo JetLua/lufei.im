4177593c7a077e13fa099008880d5876e6ec9003
无忘 <wuwang@yige-group.com>
2021-11-09 18:57:25 +0800

    update

--- src/pages/texture-packer/index.tsx
+++ src/pages/texture-packer/index.tsx
@@ -6,7 +6,7 @@ import style from './style.module.scss'
 
 export default React.memo(function() {
   return <section className={style.root}>
-    <Folder style={{backgroundColor: 'red'}}/>
+    <Folder/>
     <Canvas/>
     <Config/>
   </section>
