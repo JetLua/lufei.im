a0bd4198d71eec6bfe11a6a74bc83acb3b3b4735
无忘 <wuwang@yige-group.com>
2021-11-09 20:32:04 +0800

    update

--- src/pages/texture-packer/folder/index.tsx
+++ src/pages/texture-packer/folder/index.tsx
@@ -1,10 +1,23 @@
+import {Button} from '@mui/material'
+import {useReducer} from '~/util'
 import style from './style.module.scss'
 
 export default React.memo(function(props: Props) {
+  const [state, dispatch] = useReducer({
+    buttons: ['添加图片', '添加文件夹']
+  })
   return <section className={style.root}
     style={{...props.style}}
   >
-    folder
+    <section>
+      {
+        state.buttons.map((item, i) => {
+          return <Button variant="contained"
+            size="small"
+          >{item}</Button>
+        })
+      }
+    </section>
   </section>
 })
 
