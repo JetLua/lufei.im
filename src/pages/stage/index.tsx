3c0e543309bb1e4091117201995fe450f72e994f
无忘 <wuwang@yige-group.com>
2022-02-20 15:31:26 +0800

    upgrade dependencies

--- src/pages/stage/index.tsx
+++ src/pages/stage/index.tsx
@@ -13,9 +13,21 @@ export default React.memo(function() {
   })
 
   return <section>
-    <Player
-      autoplay loop
-      src="AnimatedSticker.json"
-    />
+    <section style={{display: 'flex', marginBottom: 16}}>
+      <div style={{flex: 1}}><img style={{width: '100%'}} src="https://cdn.shopify.com/s/files/1/0528/2486/7015/files/2282.png"/></div>
+      <div style={{flex: 1, padding: '16px 24px'}}>
+        <p><b>First</b></p>
+        <p>Apply a generous amount of <a href="https://florasis.com/products/coverage-foundation?_pos=2&_sid=e688b2e74&_ss=r">foundation</a> onto the face.</p>
+      </div>
+    </section>
+    <section>
+      <section style={{display: 'flex'}}>
+        <div style={{flex: 1}}><img style={{width: '100%'}} src="https://cdn.shopify.com/s/files/1/0528/2486/7015/files/2655.png"/></div>
+        <div style={{flex: 1, padding: '16px 24px'}}>
+          <p><b>Second</b></p>
+          <p>Use the brush directly onto face using circular motion to follow the texture of the skin and disperse the foundation evenly onto the whole face in order to cover quickly large areas.</p>
+        </div>
+      </section>
+    </section>
   </section>
 })
