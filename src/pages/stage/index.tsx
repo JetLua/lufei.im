6cf66a5369594fbea10d00e58b953cfa4eaecd2f
无忘 <wuwang@yige-group.com>
2022-03-24 13:47:20 +0800

    update

--- src/pages/stage/index.tsx
+++ src/pages/stage/index.tsx
@@ -4,7 +4,11 @@ import {useMount, useReducer} from '~/util'
 
 export default React.memo(function() {
   const [state, dispatch] = useReducer({
-
+    list: [
+      {title: 'First Step', desc: 'Place the brush vertically on the elastic net and gently pick up the powder.', img: 'https://cdn.shopify.com/s/files/1/0528/2486/7015/files/13.jpg?v=1645697258'},
+      {title: 'Second Step', desc: 'Gently rub the brush on the back of your hand to buff off any excessive powder.', img: 'https://cdn.shopify.com/s/files/1/0528/2486/7015/files/14.jpg?v=1645699853'},
+      {title: 'Third Step', desc: 'Apply the powder brush to the skin surface of the hand and spread it evenly on the face.', img: 'https://cdn.shopify.com/s/files/1/0528/2486/7015/files/15_480x480.jpg?v=1645700441'}
+    ]
   })
 
   useMount(() => {
@@ -13,21 +17,16 @@ export default React.memo(function() {
   })
 
   return <section>
-    <section style={{display: 'flex', marginBottom: 16}}>
-      <div style={{flex: 1}}><img style={{width: '100%'}} src="https://cdn.shopify.com/s/files/1/0528/2486/7015/files/2282.png"/></div>
-      <div style={{flex: 1, padding: '16px 24px'}}>
-        <p><b>First</b></p>
-        <p>Apply a generous amount of <a href="https://florasis.com/products/coverage-foundation?_pos=2&_sid=e688b2e74&_ss=r">foundation</a> onto the face.</p>
-      </div>
-    </section>
-    <section>
-      <section style={{display: 'flex'}}>
-        <div style={{flex: 1}}><img style={{width: '100%'}} src="https://cdn.shopify.com/s/files/1/0528/2486/7015/files/2655.png"/></div>
-        <div style={{flex: 1, padding: '16px 24px'}}>
-          <p><b>Second</b></p>
-          <p>Use the brush directly onto face using circular motion to follow the texture of the skin and disperse the foundation evenly onto the whole face in order to cover quickly large areas.</p>
-        </div>
-      </section>
-    </section>
+    {
+      state.list.map((item, i) => {
+        return <section key={i} style={{display: 'flex', marginBottom: 16, alignItems: 'center'}}>
+          <div style={{flex: 1}}><img style={{width: '100%', margin: 0}} src={item.img}/></div>
+          <div style={{flex: 1, padding: '16px 24px'}}>
+            <p><b>{item.title}</b></p>
+            <p>{item.desc}</p>
+          </div>
+        </section>
+      })
+    }
   </section>
 })
