e6d97898bad66745795695c1089d2a9056d50fe0
JetLu <i@lufei.im>
2022-07-15 17:48:24 +0800

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,6 +37,17 @@ export default React.memo(function({Component, pageProps}: AppProps) {
       <script dangerouslySetInnerHTML={{
         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KJS8J7M');`
       }}/>
+      <style type="text/tailwindcss"
+        dangerouslySetInnerHTML={{
+          __html: `
+            @layer utilities {
+              .max-w-500 {
+                max-width: 500px;
+              }
+            }
+          `
+        }}
+      />
     </Head>
     <context.context.Provider value={{user: state.user, dispatch}}>
       <React.Fragment>
--- src/pages/entry/index.tsx
+++ src/pages/entry/index.tsx
@@ -46,18 +46,18 @@ export default React.memo(function() {
 
   return <section>
     <div className="text-center py-2 lg:py-4 transition-all text-lg text-teal-900">
-      <IconButton disabled={state.year === 1900} onClick={() => dispatch({year: state.year - 1})}><KeyboardDoubleArrowLeftRounded/></IconButton>
+      <IconButton disabled={state.year === 1901} onClick={() => dispatch({year: state.year - 1})}><KeyboardDoubleArrowLeftRounded/></IconButton>
       <IconButton
         className="mr-2"
         onClick={() => {
-          if (state.month === 1) return dispatch({year: state.year - 1, month: 12})
+          if (state.month === 1) return dispatch({year: Math.max(state.year - 1, 1901), month: 12})
           dispatch({month: state.month - 1})
         }}
       ><KeyboardArrowLeftRounded/></IconButton>
       <Select variant="standard" value={state.year}
         onChange={e => dispatch({year: +e.target.value})}
       >
-        {Array.from({length: 201}, (_, i) => <MenuItem key={i} value={1900 + i}>{1900 + i}</MenuItem>)}
+        {Array.from({length: 201}, (_, i) => <MenuItem key={i} value={1901 + i}>{1901 + i}</MenuItem>)}
       </Select>
       <Select variant="standard" value={state.month}
         onChange={e => dispatch({month: +e.target.value})}
@@ -68,20 +68,20 @@ export default React.memo(function() {
       <IconButton
         className="ml-2"
         onClick={() => {
-          if (state.month === 12) return dispatch({year: state.year + 1, month: 1})
+          if (state.month === 12) return dispatch({year: Math.min(2100, state.year + 1), month: 1})
           dispatch({month: state.month + 1})
         }}
       ><KeyboardArrowRightRounded/></IconButton>
       <IconButton disabled={state.year === 2100} onClick={() => dispatch({year: state.year + 1})}><KeyboardDoubleArrowRightRounded/></IconButton>
     </div>
-    <section className="max-w-full transition-all grid grid-cols-7 md:max-w-screen-md mx-auto md:border md:rounded-lg overflow-hidden md:shadow-2xl" style={{maxWidth: 500}}>
+    <section className="max-w-full transition-all grid grid-cols-7 md:max-w-500 mx-auto md:border md:rounded-lg overflow-hidden md:shadow-2xl">
       {Array.from({length: 7}, (_, i) => {
         return <div key={i}
           className="text-center bg-teal-500 text-white py-2"
         >{weekNames[i]}</div>
       })}
 
-      {days.map(({day, IDayCn, IMonthCn, festival, lunarFestival, isToday}, i) => {
+      {days.map(({day, cYear, cMonth, IDayCn, IMonthCn, festival, lunarFestival, isToday}, i) => {
         const color = lunarFestival ? 'text-rose-500' :
           festival ? 'text-sky-500' : 'text-stone-400'
         const child = <React.Fragment>
@@ -90,7 +90,8 @@ export default React.memo(function() {
             {lunarFestival ? lunarFestival : festival ? festival : IDayCn === '初一' ? IMonthCn : IDayCn}
           </p>
         </React.Fragment>
-        return <div key={i} className={`text-center py-2 md:px-4 transition-all hover:bg-lime-100 cursor-pointer ${isToday ? 'bg-teal-100' : ''}`}>
+        const key = cYear ? `${cYear}${cMonth}${day}` : i
+        return <div key={key} className={`text-center py-2 md:px-4 transition-all hover:bg-lime-100 cursor-pointer ${isToday ? 'bg-teal-100' : ''} ${cYear ? '' : 'invisible'}`}>
           {day ? child : ''}
         </div>
       })}
